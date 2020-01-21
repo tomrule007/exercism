f732129fa30048f63d5c381c8ad85a0094a06a14
Thomas Herzog <Tomrule007@gmail.com>
2020-01-21 08:35:34 -0800

    Complete: javascript/pangram v2

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -2,33 +2,7 @@ export const isPangram = sentence => {
   if (!sentence) return false;
 
   const lowerCaseSentence = sentence.toLowerCase();
+  const ALPHABET = [...'abcdefghijklmnopqrstuvwxyz'];
 
-  return [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-  ].every(letter => lowerCaseSentence.includes(letter));
+  return ALPHABET.every(letter => lowerCaseSentence.includes(letter));
 };
