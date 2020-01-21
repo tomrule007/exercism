2b7b79ddae29c8478044baef95b71dce8b74991a
Thomas Herzog <Tomrule007@gmail.com>
2020-01-20 22:04:13 -0800

    Complete: javascript/space-age v2

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,14 +1,19 @@
 export const age = (planet, ageInSeconds) => {
-  const earthYearInSeconds = 31557600;
-  const orbitalPeriod = {
-    mercury: 0.2408467 * earthYearInSeconds,
-    venus: 0.61519726 * earthYearInSeconds,
-    earth: 1 * earthYearInSeconds,
-    mars: 1.8808158 * earthYearInSeconds,
-    jupiter: 11.862615 * earthYearInSeconds,
-    saturn: 29.447498 * earthYearInSeconds,
-    uranus: 84.016846 * earthYearInSeconds,
-    neptune: 164.79132 * earthYearInSeconds
+  const SECONDS_IN_YEAR = 31557600;
+  const ORBITAL_PERIODS_IN_YEARS = {
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    earth: 1,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
   };
-  return Math.round((ageInSeconds / orbitalPeriod[planet]) * 100) / 100;
+  return (
+    Math.round(
+      (ageInSeconds / (ORBITAL_PERIODS_IN_YEARS[planet] * SECONDS_IN_YEAR)) *
+        100
+    ) / 100
+  );
 };
