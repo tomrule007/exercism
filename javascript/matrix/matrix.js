04bf5e77d499aa3f73bc97a4704baa8308fc440b
Thomas Herzog <Tomrule007@gmail.com>
2020-01-26 08:40:59 -0800

    Complete: javascript/matrix v3

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -2,17 +2,25 @@ const clone = item => JSON.parse(JSON.stringify(item));
 export class Matrix {
   constructor(matrixString) {
     this.matrixString = matrixString;
+    this.rowData;
+    this.columnData;
   }
 
   get rows() {
-    return clone(
-      this.matrixString
+    if (this.rowData === undefined) {
+      this.rowData = this.matrixString
         .split('\n')
-        .map(rowString => rowString.split(' ').map(Number))
-    );
+        .map(rowString => rowString.split(' ').map(Number));
+    }
+
+    return clone(this.rowData);
   }
 
   get columns() {
-    return clone(this.rows[0].map((_, c) => this.rows.map(r => r[c])));
+    if (this.columnData === undefined) {
+      this.columnData = this.rows[0].map((_, c) => this.rows.map(r => r[c]));
+    }
+
+    return clone(this.columnData);
   }
 }
